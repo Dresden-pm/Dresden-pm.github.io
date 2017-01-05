c194657ac50b483c3f6461191090f85e80a87d46
Dominic Sonntag <dominic.sonntag@sonntag.it>
2017-01-06 00:05:41 +0100

    improved html output folder

--- converter.pl
+++ converter.pl
@@ -8,6 +8,6 @@ my $template = read_file 'index.tt';
 
 for my $file ( File::Find::Rule->file()->name('*.md')->in( $ARGV[0] ) ) {
     my $html = markdown( scalar read_file($file) );
-    write_file $file =~ s/\.md$/.html/r, $template =~ s/##content##/$html/r;
+    write_file $file =~ s/^.*?\/(\w+)\.md$/$1.html/r, $template =~ s/##content##/$html/r;
 }
 
