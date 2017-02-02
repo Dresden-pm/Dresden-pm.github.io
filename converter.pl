3afac8a0bba71ace2fceccfdc3c54105d20d5467
Dominic Sonntag <dominic.sonntag@sonntag.it>
2017-02-02 20:58:22 +0100

    added some debugging output when building

--- converter.pl
+++ converter.pl
@@ -1,5 +1,6 @@
 use strict;
 use warnings;
+use feature 'say';
 use Text::Markdown 'markdown';
 use File::Slurp;
 use File::Find::Rule;
@@ -7,6 +8,7 @@ use File::Find::Rule;
 my $template = read_file 'index.tt';
 
 for my $file ( File::Find::Rule->file()->name('*.md')->in( $ARGV[0] ) ) {
+	say "converting markdown file $file ...";
     my $html = markdown( scalar read_file($file) );
     write_file $file =~ s/^.*?\/(\w+)\.md$/$1.html/r, $template =~ s/##content##/$html/r;
 }
